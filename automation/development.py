280f906f5717fccb5d6469ef543e8fc1a82dc3c2
SarthakRawat13 <sarthak@aicraft.com.au>
2024-10-24 14:36:35 +1030

    pushing build code

--- automation/development.py
+++ automation/development.py
@@ -2,10 +2,56 @@ import json
 import os
 import sys
 import subprocess
-import shutils
+try:
+    import yaml
+except ImportError:
+    print("PyYAML is not installed. Installing it now...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pyyaml"])
+    print("PyYAML has been installed successfully.")
+import yaml
+    
+
+MAIN_DIR = "flexbuild_lsdk2108_github"
+flex_zip = "flexbuild_lsdk2108_github.tgz"
+json_file_path = 'file.json'
+required_field = ['atf', 'rcw', 'dt_uboot','dt_linux','board','defconfig','lsconfig','sdk'] 
+require_folder = "require_files"
+bld_checks_file = []
+
+def extract_flex():
+    if not is_file_present2(f"{MAIN_DIR}/setup.env"):
+        if is_file_present(os.path.join(parent_loc(),flex_zip)):
+            print("Extracting zip file")
+            command = f"tar -xvzf {os.path.join(parent_loc(),flex_zip)} -C {parent_loc()}"
+            execute_command(command = command)
+            print("Extracted zip file")
+        else:
+            print("flexbuild_lsdk2108_github.tgz file not present please download it")
+            sys.exit(1)
+
+def init_flex():
+    bool = execute_command(command = f'source {MAIN_DIR}/setup.env')
+    if bool:
+        print("Successfully init flex builder")
+    else:
+        print("init flex builder unscuccessfull")
+        sys.exit(1)
+        
+# Function to verify if file is present        
+def is_file_present2(file):
+    if os.path.exists(file):
+        return True
+    print(f"{file} not present. Please make it available")
+    return False
+    
+def parent_loc():
+    current_directory = os.getcwd()
+    parent_directory="/".join(current_directory.rsplit('/')[:-1])
+    return parent_directory
+
 
 # Function for confirmation
-def confirmation(note = "Press y to proceed and n to terminate"):
+def confirmation(note = "Press y/Y To proceed\n n/N To terminate"):
     while True:
         key_pressed = input(note)
         if key_pressed == 'y' or key_pressed == 'Y':
@@ -38,9 +84,10 @@ def read_json(file_path):
 # Function to check required parameters in the JSON data
 def check_required_params(data, required_params):
     if isinstance(data, dict):
-        missing_params = [param for param in required_params if param not in data]
+        missing_params = [param for param in required_params if param not in data or data[param] == ""]
         if missing_params:
-            return f"Missing parameters: {', '.join(missing_params)}"
+            print(f"Missing params: {required_params[missing_params]}")
+            sys.exit(1)
         else:
             return "All required parameters are present."
     else:
@@ -49,6 +96,7 @@ def check_required_params(data, required_params):
 
 # Function to execute Linux command and display output in real-time
 def execute_command(command):
+    print(f"Executing {command}")
     try:
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         # Iterate over the output line by line. Print the output in real-time
@@ -71,18 +119,9 @@ def execute_command(command):
         print(f"Error occurred: {e}")
         sys.exit(1)
 
-# Add error detection logic if some command fails then what
-# also add optee False
-def scratch_build(json_data):
-    try:
-        board = json_data['board']
-    except:
-        board = "armv8"
-    
-    if board == "":
-        board = "armv8"
-    
-    print(f"You have selected {board} for the build")
+
+def scratch_build(board = "ls1046ardb"):
+    print(f"Board: {board} is seleted for the default/scratch build")
     
     command = f'flex-builder -c rcw -m {board}' 
     execute_command(command)
@@ -90,86 +129,156 @@ def scratch_build(json_data):
     command = f'flex-builder -c atf -m {board} -b qspi'
     execute_command(command)
     
-    command = 'rm -rf ../build/firmware/u-boot/ls1046afrwy/'
-    execute_command(command)
-    
-    command = f'flex-builder -i mkfw -m {board} -b qspi'
-    execute_command(command)
-    
     command = f'bld -m {board}'
     execute_command(command)
     
-    #run change optee
-    pass
+
+def optee(value, targetdirectory):
+    with open(os.path.join(targetdirectory,'configs/sdk.yml'), 'r') as file:
+        data = yaml.safe_load(file)
+        
+    data['CONFIG_APP']['OPTEE'] = value 
+
+    with open(os.path.join(targetdirectory,'configs/sdk.yml'), 'w') as file:
+        yaml.dump(data, file, default_flow_style=False)
+    
     
 
+def custom_build(board = "ls1046ardb", targetdirectory=MAIN_DIR, firmware=False):
+    print(f"Board: {board} is seleted for the build")
+    
+    command = f'flex-builder -c rcw -m {board}' 
+    execute_command(command)
+    
+    command = f'flex-builder -c atf -m {board} -b qspi'
+    execute_command(command)
+    
+    if firmware:
+        optee('n', targetdirectory)
+        command = f'flex-builder -i mkfw -m {board} -b qspi'
+        execute_command(command)
+        optee('y', targetdirectory)
+        return
+    
+    optee('y', targetdirectory)
+    command = f'bld -m {board}'
+    execute_command(command)
+
+
+
+def flex_checks(MAIN_DIR):
+    bool_check = False
+    path = os.join.path(MAIN_DIR, 'components','linux', 'linux')
+    if not os.path.exists(path):
+        print(f'PAth doen not exists {path}')
+        bool_check = True
+        
+    path = os.join.path(MAIN_DIR, 'components','firmware','uboot')
+    if not os.path.exists(path):
+        print(f'PAth doen not exists {path}')
+        bool_check = True
+
+    path = os.join.path(MAIN_DIR, 'components','firmware','rcw')
+    if not os.path.exists(path):
+        print(f'PAth doen not exists {path}')
+        bool_check = True
+        
+    path = os.join.path(MAIN_DIR, 'components','firmware','atf')
+    if not os.path.exists(path):
+        print(f'PAth doen not exists {path}')
+        bool_check = True
+        
+    path = os.join.path(MAIN_DIR, 'configs','linux')
+    if not os.path.exists(path):
+        print(f'PAth doen not exists {path}')
+        bool_check = True
+        
+    path = os.join.path(MAIN_DIR, 'configs','sdk.cfg')
+    if not os.path.exists(path):
+        print(f'PAth doen not exists {path}')
+        bool_check = True
+        
+    if bool_check:
+        print("Please rerun the flexbuilder from scratch")
+        sys.exit(1)
+
 if __name__ == "__main__":
-    json_file_path = './file.json'
-    required_files = ['param1', 'param2', 'param3'] 
-    require_folder = "./require_file"
-    bld_checks_file = []
+    MAIN_DIR = os.path.join(parent_loc(), MAIN_DIR)
     
     #Description for the script
-    print(f"This automated script is built for ls1046ARDB for kernel version 5.10 \n\n")
-    confirmation(note = "Press y to proceed and n to terminate if you understood the message")
-    print("")
+    print(f"This automated script is built for AICRAFT pulsar\n\n")
+    confirmation(note = "Press\n y/Y to proceed\n n/N to terminate\n")
+    print()
     
+    # build check
+    print(f"Is this your first build? Means is the a build from scratch")
+    confirm = confirmation(note = "Press\n y/Y For first/default build\n n/N Custom build")
+    parent_dir = parent_loc()
+    print()  
     
-    # Cross check if files are present or not
-    print(f"Did you place each and every updated file in the {require_folder} folder")
-    print(f"The files are:")
-    for number, file in enumerate(required_files):
-        print(f"  {number+1}.  {file}")
-    confirmation(note = "Press y to proceed and n to terminate if you have placed the files in the required folder")
-    print("")
-    
+    if confirm:
+        print("This is a default/scratch build")
+        scratch_build()
+        print("Build process is completed")    
+        sys.exit(1)
+
+
+    print("This is a custom build")
+    print(f"Make sure you have placed all the files in {require_folder} folder")
+    confirmation(note = "Press\n y/Y to proceed\n n/N to terminate\n")
     
     # Cross check if files are present or not
     print(f"Performing checks to see if files are present")
-    count = 1
     is_file_present(json_file_path)
+    print()
     
     # Read the JSON file
+    # count = 0
     json_data = read_json(json_file_path)
-    if isinstance(json_data, dict):
-        result = check_required_params(json_data, required_files)
-        print(result)
-    else:
-        print(json_data)
+    # if isinstance(json_data, dict):
+    #     result = check_required_params(json_data, required_field)
+    #     print(result)
+    # else:
+    #     print(f"file is not a json file")
+    #     sys.exit(1)
 
-    is_file_present(require_folder)
-    for file in required_files:
-        is_file_present(os.path.join(require_folder, file))
-        count+=1
-    if count!=len(required_files):
-        print(f"Mismatch file counts: Count: {count}    file counts: {len(required_files)}")
-        sys.exit(1)
-    count = 1
-    for file in required_files:
-        if file not in json_data.keys():
-            print(f"key: {file} not present please fill that")
-            sys.exit(1)
-    print(f"All files are present \n")
+    # # Verifing each and every files
+    # is_file_present(require_folder)
+    # for file_ in json_data:
+    #     is_file_present(os.path.join(require_folder, file_))
+    # print(f"All files are present \n")
     
+    #Getting board from user file.json
+    try:
+        board = json_data['board']
+        if board == "":
+            board = "ls1046ardb"
+    except:
+        board = "ls1046ardb"
+        
 
-    # build check
-    print(f"Is this your first build? Means is the a build fro scratch")
-    confirm = confirmation(note = "Press y if this is the first build for the first time and n if this is not te first time")
-    print("")  
+    print("Copying all the files")
+    command = f"cp -r ./{require_folder} {MAIN_DIR}"
+    execute_command(command)
+    print("/n")
+
+    #Perfrom checks first to flexbuilder folder
+    flex_checks(MAIN_DIR)
     
+    print(f"Build process")
+    confirmation(note = "Press\n y/Y to proceed\n n/N to terminate\n")
     
-    if confirm:
-        scratch_build()
+    #board process for firmware
+    bool_check = input("Do you want to Build firmware only\nPress\n y/Y to proceed\n Any other key to skip\n")
+    if bool_check:
+        custom_build(board = board, targetdirectory = MAIN_DIR, firmware=True)
     
-    # perform components checks if fails give option to rerun from scratch or exit
-    for file in bld_checks_file:
-        is_file_present(file)
+    print(f"Whole build process which includes rootfs, bootfs and firmware")
+    bool_check = input("Do you want to Build whole OS\nPress\n y/Y to proceed\n Any other key to skip\n")
+    if bool_check:
+        custom_build(board = board, targetdirectory = MAIN_DIR, firmware=False)
         
-        
-    # Copy file to each location
-#    shutil.copy(source, destination)
-    
-    
+    print("Build process is completed")    
     
     
     
